83fdec931f6fb7f0d7779a321dee1b96b80964fd
Lexaa19 <caliopi.durdunescu@yahoo.com>
2025-09-05 15:27:00 +0300

    Misc changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import LiquidChrome from "./LiquidChrome";
+// import the userNavigate hook
+// used to let you programatically navigate to another route after a button is
 import { useNavigate } from "react-router-dom";
+import MagicCard from './MagicCardEffect';
+import './styles/magicCardEffect.css';
+import HomepageTextEffect from "./HomepageTextEffect";
+import StatisticsSection from './StatisticsSection';
 
 function App() {
-const navigate = useNavigate();
 
+const navigate = useNavigate();
   return (
     <div className="home-bg">
-      {/* Liquid Chrome Effect */}
+      {/* Liquid Chrome Background Effect */}
     <div className="liquidchrome-bg">
       <LiquidChrome
         baseColor={[0.8, 0.75, 0.95]}
@@ -20,16 +25,34 @@ const navigate = useNavigate();
     </div>
       {/* Overlay for readability */}
       <div className="homepage-overlay"></div>
-        <div className="hero-content" style={{ position: "relative", zIndex: 2 }}>
-
+        <div className="hero-content" >
         <div className="hero-logo">🧬</div>
-        <h1 className="hero-title">Gene Sphere</h1>
-        <h2 className="hero-sub">Advanced Genetics Platform</h2>
-        <div className="hero-desc">
-          Explore, analyze, and visualize genetic data with cutting-edge tools.
-        </div>
-       
+        <HomepageTextEffect
+          text="Gene Sphere"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          className="text-2xl mb-8 hero-title"
+        />
+        <h2 className="hero-sub">Explore Cancer Genomics Data</h2>
+      <div className="hero-cards-container">
+      <StatisticsSection />
+      <div className="hero-cards">
+        <MagicCard
+          textAutoHide={true}
+          enableStars={true}
+          enableSpotlight={true}
+          enableBorderGlow={true}
+          enableTilt={true}
+          enableMagnetism={true}
+          clickEffect={true}
+          spotlightRadius={300}
+          particleCount={12}
+          glowColor="132, 0, 255"
+        />
       </div>
+    </div>
+    </div>
     </div>
   );
 }
